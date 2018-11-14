691e9b7a52574cdb7ef1b1cf149afd9522254348
Matt Zuzolo <mattzuzolo@outlook.com>
2018-11-13 23:52:20 -0500

    Logout mutation built

--- server/schema/mutations.js
+++ server/schema/mutations.js
@@ -23,6 +23,16 @@ const mutation = new GraphQLObjectType({
         //GraphQL will wait to return it
         return AuthService.signup({ email, password, req })
       }
+    },
+    logout: {
+      type: UserType,
+      resolve(parentValue, args, req){
+        //save reference to user on req object for Passport
+        const { user } = req;
+        //Must have reference to user property on req object for passport logout to work
+        req.logout();
+        return user;
+      }
     }
   }
 });
